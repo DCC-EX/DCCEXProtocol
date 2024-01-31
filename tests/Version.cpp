aa8eddf94b38aba25d020dceaa491395b7282b88
Vincent Hamp <vincent.hamp@higaski.at>
2024-01-31 13:44:39 +0100

    Add unit test example

---
+++ tests/Version.cpp
@@ -0,0 +1,7 @@
+#include "DCCEXProtocolTest.hpp"
+
+TEST_F(DCCEXProtocolTest, version) {
+  _stream << "<iDCCEX V-1.2.3 / MEGA / STANDARD_MOTOR_SHIELD / 7>";
+  EXPECT_CALL(_delegate, receivedServerVersion(1, 2, 3)).Times(Exactly(1));
+  _dccexProtocol.check();
+}
