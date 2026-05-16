053e1b081e80f46b42f328f838af7eb19c89245c
Mathew Winters <mathew@winters.org.nz>
2026-05-16 14:27:54 +1200

    remove config.h

--- examples/ESP-IDF/main/config.h
+++
@@ -1,7 +0,0 @@
-// Example user configuration to use with the DCCEXProtocol examples
-// Copy this file and rename to config.h
-// Replace the variables below with the appropriate values for your environment
-const char *ssid = "WINTERS";         // WiFi SSID name here
-const char *password = "tesskatt"; // WiFi password here
-const char *serverAddress = "192.168.1.6";   // IP address of your EX-CommandStation
-int serverPort = 2560;                       // Network port of your EX-CommandStation
