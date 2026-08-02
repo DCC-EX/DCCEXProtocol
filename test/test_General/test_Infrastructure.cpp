7b4c3024b7126e81c54cc140bdc1058a48e94dab
peteGSX <peteracole@outlook.com.au>
2026-08-03 05:27:15 +1000

    Buffer and parser tests

--- test/test_General/test_Infrastructure.cpp
+++ test/test_General/test_Infrastructure.cpp
@@ -28,14 +28,45 @@
  * @brief Ensure the buffer is cleared when full
  */
 TEST_F(DCCEXProtocolTests, clearBufferWhenFull) {
-  // Fill buffer with garbage
-  for (size_t i{0}; i < 500; ++i)
-    _stream.write(static_cast<uint8_t>('A' + (random() % 26)));
+  // Fill the input buffer with garbage (operator<< feeds the input side)
+  _stream << std::string(500, 'A');
 
   // Proceed with normal message
   _stream << R"(<m "Hello World">)";
   EXPECT_CALL(_delegate, receivedMessage(StrEq("Hello World"))).Times(Exactly(1));
   _dccexProtocol.check();
+}
+
+/**
+ * @brief Ensure a command longer than the command buffer is dropped without crashing
+ */
+TEST_F(DCCEXProtocolTests, overlongCommandDropped) {
+  // Construct a protocol with a small command buffer to isolate the buffer-full path
+  DCCEXProtocol smallBuffer(20, 100);
+  smallBuffer.setDelegate(&_delegate);
+  smallBuffer.connect(&_stream);
+
+  // Feed a command that exceeds the 20 byte buffer (22 bytes including '>')
+  _stream << "<jR 1 2 3 4 5 6 7 8 9>";
+  EXPECT_CALL(_delegate, receivedRosterList()).Times(0);
+  smallBuffer.check();
+  EXPECT_FALSE(smallBuffer.receivedRoster());
+}
+
+/**
+ * @brief Ensure a command with more parameters than maxCommandParams is dropped without crashing
+ */
+TEST_F(DCCEXProtocolTests, commandExceedingMaxParamsDropped) {
+  // Construct a protocol with a small maximum parameter count to isolate the max-params path
+  DCCEXProtocol smallParams(500, 10);
+  smallParams.setDelegate(&_delegate);
+  smallParams.connect(&_stream);
+
+  // Feed a command with 12 parameters (more than the max of 10)
+  _stream << "<jR 1 2 3 4 5 6 7 8 9 10 11>";
+  EXPECT_CALL(_delegate, receivedRosterList()).Times(0);
+  smallParams.check();
+  EXPECT_FALSE(smallParams.receivedRoster());
 }
 
 /**
